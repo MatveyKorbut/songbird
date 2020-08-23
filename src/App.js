56045b917d69540fcd88ed58af58710e10227351
Matvey Korbut <kmat@gurtam.com>
2020-08-23 13:17:16 +0300

    feat: ListMenu component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Header from "./Components/Header/Header";
+import ListMenu from "./Components/ListMenu/ListMenu";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       score: 0,
+      stage: 0
     };
   }
 
@@ -16,6 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header score={this.state.score}/>
+        <ListMenu stage={this.state.stage}/>
       </div>
     );
   }
