2b219492900d6e83bf48e088e3c5a7be0faf43d2
Matvey Korbut <kmat@gurtam.com>
2020-09-01 22:56:20 +0300

    add build

---
+++ build/precache-manifest.4bbf9730342211bcb740aaa9739ccfe0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136cb8f91f4e4a359087687dbf4911d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b8e48b94b31ecdd9c3f",
+    "url": "/static/css/main.d3b5c667.chunk.css"
+  },
+  {
+    "revision": "2de63a07ea08958f53bc",
+    "url": "/static/js/2.11e138ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.11e138ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8e48b94b31ecdd9c3f",
+    "url": "/static/js/main.9ad49b0c.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "/static/media/SongBirdLogo.4f82cd73.svg"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/defaultBird.06a46938.jpg"
+  },
+  {
+    "revision": "bffd1a6f63b2438686b32d9e4cd23001",
+    "url": "/static/media/error.bffd1a6f.mp3"
+  },
+  {
+    "revision": "3d051faa3d70641a862d69503668d499",
+    "url": "/static/media/gameOver.3d051faa.mp3"
+  },
+  {
+    "revision": "0022a5bd962f57f9bd4193bf87e44fb6",
+    "url": "/static/media/win.0022a5bd.mp3"
+  }
+]);
